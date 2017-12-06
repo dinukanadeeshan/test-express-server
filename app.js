644e06a1ce0645e9b6e4e48c6c200f22e3ea5cf0
dinukanadeeshan <dinuka.nadeeshan1993@gmail.com>
2017-12-06 15:05:04 +0530

    created with post requests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/v1/', (req, res) => res.send('Hello World!'))
+app.post('/api/v2/', (req, res) => {
+  console.log(req);
+  res.send('Recieved')
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
